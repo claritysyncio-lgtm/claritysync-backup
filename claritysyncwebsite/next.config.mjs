186469b54033ee2dbb814ecb60ceed382e690555
claritysyncio-lgtm <claritysync.io@gmail.com>
2025-09-18 17:06:00 -0400

    Backup at 2025-09-18 17:05:59

--- claritysyncwebsite/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Ensure Next uses the project folder as the root for tracing
-  outputFileTracingRoot: process.cwd(),
-};
-
-export default nextConfig;
