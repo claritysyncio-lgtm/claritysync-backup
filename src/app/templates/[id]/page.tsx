6bade902ecca1d433093093741b2399ada2b4e33
claritysyncio-lgtm <claritysync.io@gmail.com>
2025-09-18 22:17:18 -0400

    Fix [id] route typing with minimal sync component

--- src/app/templates/[id]/page.tsx
+++ src/app/templates/[id]/page.tsx
@@ -1,9 +1,5 @@
 // src/app/templates/[id]/page.tsx
 
 export default function TemplatePage({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      {params.id}
-    </div>
-  );
+  return <div>{params.id}</div>;
 }
